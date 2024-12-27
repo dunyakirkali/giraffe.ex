d59f19c4b80e303e830d28063f2b781573be4e4c
Dunya Kirkali <dunyakirkali@gmail.com>
2024-12-27 09:56:02 +0100

    Setup mix settings

--- mix.exs
+++ mix.exs
@@ -7,8 +7,25 @@ defmodule Giraffe.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
+  end
+
+  def package do
+    [
+      files: ["test", "lib", "mix.exs", "README.md", "LICENSE*"],
+      maintainers: ["Dunya Kirkali"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/dunyakirkali/giraffe.ex"}
+    ]
+  end
+
+  def description do
+    """
+    Giraffe is a graph library for Elixir.
+    """
   end
 
   # Run "mix help compile.app" to learn about applications.
